0f53f2dcacf4b9eac5b8f45f708aab066e673d4d
gdberrio <guilherme.diazberrio@gmail.com>
2023-03-18 16:40:44 +0000

    code clean up: removed unused declaration of Discord Adapter

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,7 +4,6 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
