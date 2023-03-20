5c4e0ea2dc524fdbf9007cf0f3ef6afd78730452
gdberrio <guilherme.diazberrio@gmail.com>
2023-03-20 07:33:13 +0000

    exchange code for access token done

--- src/server/api/routers/truelayer.ts
+++ src/server/api/routers/truelayer.ts
@@ -3,11 +3,11 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 import { env } from "~/env.mjs";
 
 interface exchangeCodeResponse {
-    access_token: string;
-    expires_in: number;
-    token_type: string;
-    refresh_token: string;
-    scope: string;
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+  refresh_token: string;
+  scope: string;
 }
 
 export const truelayerRouter = createTRPCRouter({
@@ -18,21 +18,31 @@ export const truelayerRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ input }) => {
-      const params = new URLSearchParams();
-      params.set("grant_type", "authorization_code");
-      params.set("client_id", env.TRUELAYER_CLIENT_ID);
-      params.set("client_secret", env.TRUELAYER_CLIENT_SECRET);
-      params.set("redirect_uri", "http://localhost:3000/api/callback");
-      params.set("code", input.code);
-      const connect_URL = `${env.TRUELAYER_AUTH_API}/connect/token?${params.toString()}`;
-      const auth_request = await fetch(connect_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-      })
-        const auth_response = await auth_request.json() as exchangeCodeResponse;
+      const params = {
+        grant_type: "authorization_code",
+        client_id: env.TRUELAYER_CLIENT_ID,
+        client_secret: env.TRUELAYER_CLIENT_SECRET,
+        redirect_uri: "http://localhost:3000/callback",
+        code: input.code,
+      };
+      try {
+        const connect_URL = `${
+          env.TRUELAYER_AUTH_API
+        }/connect/token`;
+        const auth_request = await fetch(connect_URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(params),
+        });
+        const auth_response =
+          (await auth_request.json()) as exchangeCodeResponse;
         console.log(auth_response);
-        return auth_response;
+        return {auth_response};
+      } catch (error) {
+        console.log(error);
+      }
+      console.log(JSON.stringify(params));
     }),
 });
